2ea50fa3ce0d0f4c2abd6e813306f17a03e9ade5
Victor K BIDI <victor.kbidi@protonmail.com>
2017-10-31 02:25:08 +0400

    Map: change marker icon

--- src/main.js
+++ src/main.js
@@ -49,9 +49,9 @@ Vue.component('v-popup', Vue2Leaflet.Popup)
 delete L.Icon.Default.prototype._getIconUrl
 
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+  iconRetinaUrl: require('./assets/images/marker.png'),
+  iconUrl: require('./assets/images/marker.png')
+  // shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 })
 
 Vue.http.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('id_token')
